287da7f4d804b1511dd80c35ce4ff451d6d06d44
Venture143 <63887505+Venture143@users.noreply.github.com>
2020-11-12 09:29:47 -0700

    Create mod.cpp
    creates basic mod.cpp file

---
+++ Addons/mod.cpp
@@ -0,0 +1,22 @@
+/// Name of your mod
+name = "143rd DKOK Custom Factions mod";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "\Samples_F\Data_01\Images\picture.paa"; 
+/// Display next to the item added by the mod
+logoSmall = "\Samples_F\Data_01\Logos\logo_small.paa";
+/// Logo displayed in the main menu
+logo = "\Samples_F\Data_01\Logos\logo.paa";
+/// Change the logo When the mouse is over, in the main menu of Arma 3
+logoOver = "\Samples_F\Data_01\Logos\logoOver.paa";
+/// Website URL, that can accessed from the mod data
+action = "https://community.bistudio.com/wiki/Mod_Presentation";
+/// Tool tip displayed when the mouse is left over, in the main menu of Arma 3
+tooltipOwned = "adds custom, kitbashed, and ALIVE compatable factions to ARMA 3";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = {	0.23, 0.39, 0.30, 1};
+/// Overview text, displayed from the extension menu
+overview = "This mod adds a variety of factions found previously in the 143rd rebalance mod, now easier to update and with more factions";
+/// Hide the extension name 
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
