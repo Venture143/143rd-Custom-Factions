166c142d0f769af949ce84a803d9cbef4558733b
Venture143 <63887505+Venture143@users.noreply.github.com>
2020-11-24 15:33:58 -0700

    Create config.cpp

---
+++ Addons/OrkHordez/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+    class DK143_CustomFactions_OrkHordez {
+        units[] = {
+
+        };
+        weapons[] = {};
+        requiredVersion = 1.62;
+        requiredAddons[] = {"ace_common"};
+        author = "VGm. Venture";
+        authors[] = {"VGm. Venture"};
+    };
+};
+
+class CfgFactions {};
+
+class CfgGroups {};
+
+class CfgVehicles {};
