0030d8430bb95d897849ed6694602fd2f27b94ef
Venture143 <63887505+Venture143@users.noreply.github.com>
2020-11-25 09:40:06 -0700

    starting Ork Hordez
    initial Fleshout

--- Addons/OrkHordez/config.cpp
+++ Addons/OrkHordez/config.cpp
@@ -47,4 +47,56 @@ class CfgGroups {
 class CfgVehicles {
 //TODO:
 //OrkBoyz + MekBoy Contrapshuns
+    // Define Macros
+	/// Magazines macros definition ///
+	#define mag_2(a) a, a
+	#define mag_3(a) a, a, a
+	#define mag_4(a) a, a, a, a
+	#define mag_5(a) a, a, a, a, a
+	#define mag_6(a) a, a, a, a, a, a
+	#define mag_7(a) a, a, a, a, a, a, a
+	#define mag_8(a) a, a, a, a, a, a, a, a
+	#define mag_9(a) a, a, a, a, a, a, a, a, a
+	#define mag_10(a) a, a, a, a, a, a, a, a, a, a
+	#define mag_11(a) a, a, a, a, a, a, a, a, a, a, a
+	#define mag_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+
+	// ARMA 3 Base Soldier Classes
+	class O_Soldier_base_F;
+    
+    // Base Ork Profile
+    class DK143_Base_OrkBoy : O_Soldier_base_F {
+        author = "VGm. Venture";
+        scope = 0;
+        scopeCurator = 0;
+        displayName = "Ork Base Dude";
+        side = 0;
+        faction = "DK143_O_OrkHordez";
+
+        identityTypes[] = {"Ork_Head"};
+
+        uniformClass = "Boy1Uni_OP";
+    };
+    
+    // Melee Unit goes here
+    
+    // Units
+    class DK143_O_OrkBurna : DK143_Base_OrkBoy {
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Ork Burna";
+    };
+    
+    class DK143_O_OrkBooma : DK143_Base_OrkBoy {
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Ork BooMah";
+    };
+    
+    class DK143_O_OrkDakka : DK143_Base_OrkBoy {
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Ork Dakka";
+    };
+    
 };
