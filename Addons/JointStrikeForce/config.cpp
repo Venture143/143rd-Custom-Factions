dd6274c01830258ee6122be7b19c9d5f9978cf1f
Brett <Brettthompson1990@gmail.com>
2021-02-13 20:21:05 -0700

    fix 2 eletric boog

--- Addons/JointStrikeForce/config.cpp
+++ Addons/JointStrikeForce/config.cpp
@@ -35,7 +35,152 @@ class CfgFactionClasses {
 };
 
 class CfgGroups {
-  class WEST {  };
+  class WEST {
+      class B_JointStrikeForce {
+          name = "Joint Strike Force";
+
+          class Infantry {
+                name = "Infantry"; 
+
+                class B_JointStrikeForce_infantry_ghosts {
+                    name = "Ghosts";
+                    side = 1;
+                    faction = "B_B_JointStrikeForce";
+                    icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "CORPORAL";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Squad_Leader_01";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit2 {
+                        osition[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Medic_01";
+                    };
+                    class Unit4 {
+                        position[] = {-10,-10,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Sniper_01";
+                    };
+                    class Unit5 {
+                        position[] = {15,-15,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Sniper_01";
+                    };
+                    class Unit6 {
+                        position[] = {-15,-15,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit7 {
+                        position[] = {20,-20,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit8 {
+                        position[] = {-20,-20,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                };
+          
+                            class B_JointStrikeForce_infantry_pioneers {
+                    name = "Pioneers";
+                    side = 1;
+                    faction = "B_B_JointStrikeForce";
+                    icon = "\A3\ui_f\data\map\markers\nato\b_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "CORPORAL";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Squad_Leader_01";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Rifleman_01";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Medic_01";
+                    };
+                    class Unit4 {
+                        position[] = {-10,-10,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_AT_01";
+                    };
+                    class Unit5 {
+                        position[] = {15,-15,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Grenadier_01";
+                    };
+                    class Unit6 {
+                        position[] = {-15,-15,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Autorifleman_01";
+                    };
+                    class Unit7 {
+                        position[] = {20,-20,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Autorifleman_01";
+                    };
+                    class Unit8 {
+                        position[] = {-20,-20,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Grenadier_01";
+                    };
+                    class Unit9 {
+                        position[] = {25,-25,0};
+                        rank = "PRIVATE";
+                        side = 1;
+                        vehicle = "B_JointStrikeForce_Grenadier_01";
+                    };
+                };
+          };
+
+          class Mechanized {
+            name = "Mechanized Infantry";
+
+          };
+      };
+    };
 };
 
 class CfgVehicles {
